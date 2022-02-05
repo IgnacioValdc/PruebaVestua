c9b94e6b4b22c888f4b6f735ae4a02ca58f2bb8a
IgnacioValdc <ignacio.valdc@gmail.com>
2022-02-05 19:23:43 -0300

    Ejercicio 1 completado

--- question-1/README.md
+++ question-1/README.md
@@ -4,4 +4,16 @@
 
 # Razonamiento
 
-_Escribir aquí el razonamiento al puzzle_
+Para comenzar el ejercicio verifique si existia algun patron en la cantidad de formas distintas que se pueden subir las escaleras
+
+Comenzando por:
+
+N = 0       Formas: 0
+N = 1       Formas: 1
+N = 2       Formas: 2
+N = 3       Formas: 3
+N = 4       Formas: 5
+N = 5       Formas: 8
+
+Al tener los 5 primeros N note que seguian un patron especificamente que el resultado dependia de la suma de los 2 resultados anteriores
+lo cual me llevo a la sucesion de Fibonacci
--- question-1/script.js
+++ question-1/script.js
@@ -4,5 +4,23 @@
  * $ node scritp.js 100
  */
 
-const args = process.argv.slice(-1);
-console.log(`Running question #1 with args ${args}`)
+const args = process.argv.slice(-1);            //Ingresamos la variable por linea de comandos ej: node script.js "numero"
+
+// var valor = args[0];                         //Se deja esta variable en caso de querer ingresar los datos desde el codigo
+console.log(valor);
+if(args < 3){
+    console.log("Las formas distintas de subir la escalera es: "+args);
+
+}
+else{
+    let subirUno = 1;
+    let subirDos = 2;
+
+    for(let i = 2; i < args; i++){
+        const suma = subirUno + subirDos;
+        subirUno = subirDos;
+        subirDos = suma;
+    }
+    console.log("Las formas distintas de subir la escalera es: "+subirDos);
+}
+//console.log(`Running question #1 with args ${args}`)
