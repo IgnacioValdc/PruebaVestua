f055710c79dda4c15774a90c1272c21e9f412235
IgnacioValdc <ignacio.valdc@gmail.com>
2022-02-06 02:27:57 -0300

    Ejercicio 2 a medias

--- question-2/script.js
+++ question-2/script.js
@@ -5,4 +5,27 @@
  */
 
 const args = process.argv.slice(-1);
-console.log(`Running question #2 with args ${args}`)
+var fs = require('fs')
+, archivo = args[0];
+
+const data = fs.readFileSync(archivo,'UTF-8');
+const lines = data.split("\n");
+lines.shift();
+var id_usuario = [];
+var entidad_id = [];
+var tipo = [];
+var imp_click = [];
+
+
+var lineaS;
+
+for(let i = 0; i<lines.length; i++){
+    lineaS = lines[i].split(",");
+    id_usuario[i] = lineaS[0];
+    entidad_id[i] = lineaS[1];
+    tipo[i] = lineaS[2];
+    imp_click[i] = lineaS[3];
+}
+console.log(id_usuario[0]);
+
+// console.log(`Running question #2 with args ${args[1]}`)
